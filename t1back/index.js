70e0cc5a6705e3623c1017d9efc1dc60c2e4c622
HsuanYu CHOU <55382796+hy-chou@users.noreply.github.com>
2023-03-14 15:49:31 +0800

    mod index.js, Camera -> capture, add record of legal users

--- t1back/index.js
+++ t1back/index.js
@@ -5,32 +5,49 @@ const express = require('express');
 const { Server } = require('socket.io');
 
 const { authenticate } = require('./src/authenticate');
-const Camera = require('./src/Camera');
+const { capture } = require('./src/capture');
 
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
+
+const legalUsers = new Set();
 
 app.get('/', (_, res) => {
   res.sendFile(`${__dirname}/public/index.html`);
 });
 
 io.on('connection', (socket) => {
-  console.log('a user connected');
+  console.log(`${socket.id} connected`);
 
   socket.on('login', async (credentials) => {
-    const isAuthenticated = await authenticate(credentials);
+    const isLegal = await authenticate(credentials);
 
-    if (isAuthenticated) {
-      socket.emit('message', 'OK');
-      Camera.stream(socket);
+    if (isLegal) {
+      legalUsers.add(socket.id);
+      console.log(legalUsers);
+
+      socket.emit('message', 'login OK');
     } else {
-      socket.emit('message', 'KO');
+      socket.emit('message', 'login KO');
+    }
+  });
+
+  socket.on('camera', () => {
+    if (legalUsers.has(socket.id)) {
+      capture(socket);
+
+      socket.emit('message', 'camera OK');
+    } else {
+      socket.emit('message', 'camera KO');
     }
   });
 
   socket.on('disconnect', () => {
-    console.log('user disconnected');
+    legalUsers.delete(socket.id);
+    console.log(legalUsers);
+
+    console.log(`${socket.id} disconnected`);
   });
 });
 
---
+++ t1back/src/capture.js
@@ -0,0 +1,24 @@
+/* eslint-disable no-console */
+const { spawn } = require('node:child_process');
+
+const capture = (socket) => {
+  const subprocess = spawn(
+    '/usr/bin/python3',
+    ['capture.py'],
+    { stdio: ['ignore', 'pipe', 'ignore'] },
+  );
+  let image = '';
+
+  subprocess.stdout.on('data', (data) => {
+    image += data;
+  });
+
+  subprocess.on('close', async (code) => {
+    if (code === 0) {
+      socket.emit('image', `data:image/png;base64,${image.slice(2, -2)}`);
+    }
+    console.log(`child process exited with code ${code}`);
+  });
+};
+
+module.exports = { capture };

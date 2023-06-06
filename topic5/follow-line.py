8a332be2e928b8c2cc2915ca0136935729bd1bd2
HsuanYu CHOU <55382796+hy-chou@users.noreply.github.com>
2023-06-06 20:58:07 +0800

    mod follow-line, use edge detection after binary classification

--- topic5/follow-line.py
+++ topic5/follow-line.py
@@ -9,31 +9,31 @@ import RPi.GPIO as GPIO
 from adafruit_motorkit import MotorKit as Kit
 from picamera2 import Picamera2
 
-# CONSTANTS
-
 PIN_FRONT_MOTOR = 17  # 11 -> 17
 PWM_FREQ = 50
 STEP = 15
 THROTTLE_SLOW = 0.8
 THROTTLE_STOP = 0
 DC_LEFT = 5
-DC_CENTER = 7.25
 DC_RIGHT = 8.5
+DC_CENTER = (DC_LEFT + DC_RIGHT) / 2
 CROP_TOP = 0
 CROP_BOTTOM = 80
-CROP_LEFT = 100
+CROP_LEFT = 0
 CROP_RIGHT = 640 - CROP_LEFT
+IDEAL_POS = (CROP_RIGHT - CROP_LEFT) / 2
+LINE_NEAR = 0
+LINE_FAR = 10
 
 
 def signal_handler(signal, frame):
+    picam2.stop()
     kit.motor1.throttle = THROTTLE_STOP
     pwm.ChangeDutyCycle(DC_CENTER)
     time.sleep(0.3)
     GPIO.cleanup()
     sys.exit(0)
 
-
-# INIT
 
 signal.signal(signal.SIGINT, signal_handler)
 GPIO.setmode(GPIO.BCM)
@@ -43,35 +43,38 @@ pwm.start(DC_CENTER)
 kit = Kit(i2c=board.I2C())  # pi hat v.5, M2 L+R-
 picam2 = Picamera2()
 picam2.start()
-
-
-# MAIN
-
-pwm.ChangeDutyCycle(DC_CENTER)
+next_duty_cycle = DC_CENTER
 kit.motor1.throttle = THROTTLE_SLOW
 
+
 while True:
+
     im = picam2.capture_array()
     im = im[CROP_TOP:CROP_BOTTOM, CROP_LEFT:CROP_RIGHT]
+    im = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
+    im = cv2.GaussianBlur(im, (49, 49), 0)
 
-    im_b, im_g, im_r = im[:, :, 0], im[:, :, 1], im[:, :, 2]
+    _, imThsh = cv2.threshold(im, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
 
-    im_b = cv2.GaussianBlur(im_b, (5, 5), 0)
-    im_g = cv2.GaussianBlur(im_g, (5, 5), 0)
-    im_r = cv2.GaussianBlur(im_r, (5, 5), 0)
-    _, im_b = cv2.threshold(im_b, 0, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)
-    _, im_g = cv2.threshold(im_g, 0, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)
-    _, im_r = cv2.threshold(im_r, 0, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+    imCanny = cv2.Canny(imThsh, 16, 255)
+    pos = np.mean(np.nonzero(imCanny[0]))
 
-    im = np.logical_and(im_b, im_g)
-    im = np.logical_and(im, im_r) * 255
+    diff = pos - IDEAL_POS
 
-    clear = np.std(im[0])
-    # print(clear)
+    if diff < -100:
+        dc = DC_RIGHT
+    elif diff < -50:
+        dc = (DC_RIGHT + DC_CENTER) / 2
+    elif diff < -10:
+        dc = (DC_RIGHT + DC_CENTER * 2) / 3
+    elif diff > 10:
+        dc = (DC_LEFT + DC_CENTER * 2) / 3
+    elif diff > 50:
+        dc = (DC_LEFT + DC_CENTER) / 2
+    elif diff > 100:
+        dc = DC_LEFT
+    else:
+        dc = DC_CENTER
 
-    offset = np.mean(np.nonzero(im[0])) - (CROP_RIGHT - CROP_LEFT)/2
-    print(offset)
-    if offset < -10:
-        pwm.ChangeDutyCycle(DC_LEFT)
-    elif offset > 10:
-        pwm.ChangeDutyCycle(DC_RIGHT)
+    next_duty_cycle = dc
+    pwm.ChangeDutyCycle(next_duty_cycle)
